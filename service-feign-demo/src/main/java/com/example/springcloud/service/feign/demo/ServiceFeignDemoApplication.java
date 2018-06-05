968345af508be90dac61867cb93c3027d923a903
wenka <wkwenka@gmail.com>
2018-06-05 11:10:07 +0800

    断融器 hystrix dashboard

--- service-feign-demo/src/main/java/com/example/springcloud/service/feign/demo/ServiceFeignDemoApplication.java
+++ service-feign-demo/src/main/java/com/example/springcloud/service/feign/demo/ServiceFeignDemoApplication.java
@@ -1,14 +1,31 @@
 package com.example.springcloud.service.feign.demo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients //开启Feign的功能
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServiceFeignDemoApplication {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet hystrixMetricsStreamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(hystrixMetricsStreamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceFeignDemoApplication.class, args);
--- service-ribbon-demo/src/main/java/com/example/springcloud/ribbon/service/ribbon/demo/ServiceRibbonDemoApplication.java
+++ service-ribbon-demo/src/main/java/com/example/springcloud/ribbon/service/ribbon/demo/ServiceRibbonDemoApplication.java
@@ -1,25 +1,41 @@
 package com.example.springcloud.ribbon.service.ribbon.demo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient //向服务中心注册
 @EnableHystrix // 开启 hystrix
+@EnableHystrixDashboard
 public class ServiceRibbonDemoApplication {
 
     @Bean
-    @LoadBalanced //表明该 restTemplate 开启负载均衡功能
-    RestTemplate restTemplate(){
+    @LoadBalanced
+        //表明该 restTemplate 开启负载均衡功能
+    RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRibbonDemoApplication.class, args);
+    }
+
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet hystrixMetricsStreamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(hystrixMetricsStreamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }

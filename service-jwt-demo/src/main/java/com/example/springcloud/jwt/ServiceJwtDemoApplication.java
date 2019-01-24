68fa8a344a8d40e2bf3ca5e6323b58279d6bbf20
wenka <wkwenka@gmail.com>
2019-01-24 17:24:47 +0800

    JWT(json web token) 生成与解码

---
+++ service-jwt-demo/src/main/java/com/example/springcloud/jwt/ServiceJwtDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springcloud.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceJwtDemoApplication.class, args);
+	}
+
+}
+

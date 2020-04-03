cea9b6877ec676e71b78efe62f55c23089d7e86d
wenka <wkwenka@gmail.com>
2020-04-03 15:56:53 +0800

    Sqlite 10000条数据 插入(3s>time>2s)、查询(time<1s) 测试

--- springboot-sqllite-demo/src/test/java/StudentTest.java
+++ springboot-sqllite-demo/src/test/java/StudentTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,19 +23,31 @@ import java.util.UUID;
 @RunWith(SpringRunner.class)
 public class StudentTest {
 
+    private int total = 10000;
     @Autowired
     private StudentDao studentDao;
 
     @Test
     public void test() {
-        this.studentDao.save(new Student(UUID.randomUUID().toString(), "C", 22, new Date()));
+
+        List<Student> studentList = new LinkedList<>();
+        for (int i = 0; i < total; i++) {
+            studentList.add(new Student(UUID.randomUUID().toString(), "AA" + i, 22, new Date()));
+        }
+        long begin = System.currentTimeMillis();
+        this.studentDao.saveAll(studentList);
+        System.out.println("---------------------------");
+        System.out.println(System.currentTimeMillis() - begin);
     }
 
     @Test
     public void testSelect() {
+        long begin = System.currentTimeMillis();
         List<Student> all = this.studentDao.findAll();
-        for (Student student : all) {
-            System.out.println(student);
-        }
+        System.out.println("---------------------------" + all.size());
+        System.out.println(System.currentTimeMillis() - begin);
+//        for (Student student : all) {
+//            System.out.println(student);
+//        }
     }
 }

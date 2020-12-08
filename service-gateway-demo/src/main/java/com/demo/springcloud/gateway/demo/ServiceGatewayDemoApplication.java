8c4b0bc08406c085c91e92389fff347b65183d35
wenka <wkwenka@gmail.com>
2020-12-08 15:34:28 +0800

    gateway 通过注册中心实现请求转发

---
+++ service-gateway-demo/src/main/java/com/demo/springcloud/gateway/demo/ServiceGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springcloud.gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGatewayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayDemoApplication.class, args);
+    }
+
+}
